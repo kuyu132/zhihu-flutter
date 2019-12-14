55f9112303f0e92686d490398ae223a6b18fb09e
wangguoqun <wangguoqun13230@gmail.com>
2019-12-14 17:46:45 +0800

    feature:升级最新的flutter

--- ios/Runner/GeneratedPluginRegistrant.m
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -3,11 +3,36 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<barcode_scan/BarcodeScanPlugin.h>)
 #import <barcode_scan/BarcodeScanPlugin.h>
+#else
+@import barcode_scan;
+#endif
+
+#if __has_include(<flutter_webview_plugin/FlutterWebviewPlugin.h>)
 #import <flutter_webview_plugin/FlutterWebviewPlugin.h>
+#else
+@import flutter_webview_plugin;
+#endif
+
+#if __has_include(<fluttertoast/FluttertoastPlugin.h>)
 #import <fluttertoast/FluttertoastPlugin.h>
+#else
+@import fluttertoast;
+#endif
+
+#if __has_include(<permission_handler/PermissionHandlerPlugin.h>)
 #import <permission_handler/PermissionHandlerPlugin.h>
-#import <webview_flutter/WebViewFlutterPlugin.h>
+#else
+@import permission_handler;
+#endif
+
+#if __has_include(<webview_flutter/FLTWebViewFlutterPlugin.h>)
+#import <webview_flutter/FLTWebViewFlutterPlugin.h>
+#else
+@import webview_flutter;
+#endif
 
 @implementation GeneratedPluginRegistrant
 
